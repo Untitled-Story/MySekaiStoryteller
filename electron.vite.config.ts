4455dfa6c04ded14ccd1df121f7e3fcf13c0a009
Guang_Chen <guangchen23333@gmail.com>
2025-07-14 17:50:05 +0800

    fix(build): Now buildable

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -7,5 +7,32 @@ export default defineConfig({
   preload: {
     plugins: [externalizeDepsPlugin()]
   },
-  renderer: {}
+  renderer: {
+    plugins: [
+      {
+        name: 'build-html',
+        transformIndexHtml: (html) => {
+          return {
+            html,
+            tags: [
+              {
+                tag: 'script',
+                attrs: {
+                  src: 'live2d.min.js'
+                },
+                injectTo: 'body'
+              },
+              {
+                tag: 'script',
+                attrs: {
+                  src: 'live2dcubismcore.min.js'
+                },
+                injectTo: 'body'
+              }
+            ]
+          }
+        }
+      }
+    ]
+  }
 })
