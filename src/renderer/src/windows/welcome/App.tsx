fa64781a99dd9be9f823294880fd96db1be07113
Guang_Chen_ <guangchen23333@gmail.com>
2025-09-28 23:00:19 +0800

    feat(HomePage): Finish it

--- src/renderer/src/windows/welcome/App.tsx
+++ src/renderer/src/windows/welcome/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import LeftSidebar from '@windows/welcome/components/LeftSidebar'
 import { Route, Routes } from 'react-router'
+import HomePage from '@windows/welcome/pages/HomePage'
 
 function App(): React.JSX.Element {
   return (
     <>
       <LeftSidebar />
-      <main className="fixed left-65 p-6 w-full">
+      <main className="fixed top-0 left-65 right-0 bottom-0 overflow-auto">
         <Routes>
-          <Route path="/" element={<text>Home</text>} />
+          <Route path="/" element={<HomePage />} />
           <Route path="/projects" element={<text>Projects</text>} />
           <Route path="/settings" element={<text>Settings</text>} />
         </Routes>
