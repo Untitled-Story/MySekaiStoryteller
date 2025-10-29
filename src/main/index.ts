18283a15f82d79ff9e5fed22c1401478338c8abd
Guang_Chen_ <guangchen23333@gmail.com>
2025-10-29 20:48:27 +0800

    perf(Electron): 优化 Electron 性能配置

--- src/main/index.ts
+++ src/main/index.ts
@@ -18,6 +18,10 @@ const logger: Logger<ILogObj> = new Logger({
 })
 
 app.commandLine.appendSwitch('autoplay-policy', 'no-user-gesture-required')
+app.commandLine.appendSwitch('enable-gpu-rasterization')
+app.commandLine.appendSwitch('disable-renderer-backgrounding')
+app.commandLine.appendSwitch('disable-backgrounding-occluded-windows')
+app.commandLine.appendSwitch('disable-background-timer-throttling')
 
 protocol.registerSchemesAsPrivileged([
   {
