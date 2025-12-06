d319ca069ad039d7ef4752ab6a2a4843c582e265
Guang_Chen_ <guangchen23333@gmail.com>
2025-12-07 00:01:25 +0800

    chore: 新增一些Image和Models的错误提示

--- src/renderer/src/index.ts
+++ src/renderer/src/index.ts
@@ -1,12 +1,19 @@
 import main from './app/App'
+import { serializeError } from './utils/HelperUtils'
 
 function initialize(): void {
   window.addEventListener('unhandledrejection', (event) => {
-    window.electron.ipcRenderer.send('electron:on-error', event.reason)
+    if (event.reason instanceof Error) {
+      window.electron.ipcRenderer.send('electron:on-error', serializeError(event.reason))
+    }
   })
 
   window.addEventListener('DOMContentLoaded', async (): Promise<void> => {
-    await main().catch((error) => window.electron.ipcRenderer.send('electron:on-error', error))
+    await main().catch((error: unknown) => {
+      if (error instanceof Error) {
+        window.electron.ipcRenderer.send('electron:on-error', serializeError(error))
+      }
+    })
   })
 }
 
--- src/renderer/src/utils/HelperUtils.ts
+++ src/renderer/src/utils/HelperUtils.ts
@@ -1,3 +1,21 @@
 export function getRandomNumber(min: number, max: number): number {
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
+
+export function serializeError(err: unknown):
+  | {
+      name: string
+      message: string
+      stack?: string
+      cause?: unknown
+    }
+  | unknown {
+  if (!(err instanceof Error)) return err
+
+  return {
+    name: err.name,
+    message: err.message,
+    stack: err.stack,
+    cause: serializeError(err.cause)
+  }
+}
